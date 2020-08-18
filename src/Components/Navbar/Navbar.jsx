1b225300945577cc076e5402d805cbff686cac9c
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-08-18 20:13:44 +0200

    Navbar for website

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,56 +7,57 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <>
-      <div className="container-fluid nav_bg">
-        <div className="row">
-          <div className="col-10 mx-auto">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <div className="container-fluid">
-                <NavLink to="/" className="navbar-brand">
-                  Animation-Pg
-                </NavLink>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
+      <div id="navbar" className="fixed-top">
+        <div className="container-fluid d-flex">
+          <div className="logo mr-auto">
+            <NavLink to="/" className="navbar-brand">
+              <h1 className="text-dark">
+                <span>Koddax</span>
+              </h1>
+            </NavLink>
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="nav-menu d-none d-lg-block ">
+            <ul>
+              <li className="nav-item ">
+                <NavLink
+                  activeClassName="menu_active"
+                  exact
+                  to="/"
+                  aria-current="page"
+                  className="nav-link active"
                 >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                      <NavLink
-                        activeClassName="menu_active"
-                        to="/"
-                        className="nav-link active"
-                        aria-current="page"
-                      >
-                        Home
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink activeClassName="menu_active" to="/service" className="nav-link">
-                        Services
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink activeClassName="menu_active" to="/about" className="nav-link">
-                        About
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink to="/contact" className="nav-link">
-                        Contact
-                      </NavLink>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </nav>
+                  Home
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink activeClassName="menu_active" to="/service" className="nav-link">
+                  Services
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink activeClassName="menu_active" to="/about" className="nav-link">
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink activeClassName="menu_active" to="/contact" className="nav-link">
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
