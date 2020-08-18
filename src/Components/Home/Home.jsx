be4f944f032316156c0e2754a0252bbe5ab1bcef
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-08-18 20:10:21 +0200

    Home page with styles added, responsive for mobile & other devices

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,36 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+//import { NavLink } from 'react-router-dom';
 import web from '../../images/hero-img.svg';
-
+import Common from '../Common';
 // style
 import './Home.css';
 
 const Home = () => {
   return (
     <>
-      <section id="home" className="d-flex align-items-center">
-        <div className="container">
-          <div className="row">
-            <div className="col-10 mx-auto">
-              <div className="row">
-                <div className="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1">
-                  <h1>
-                    A much better digital experience with
-                    <strong className="brand-name"> Koddax</strong>
-                  </h1>
-                  <h2>We are a team of talented developers making websites </h2>
-                  <NavLink to="/service" className="btn-get-started scrollto">
-                    Get Started
-                  </NavLink>
-                </div>
-                <div className="col-lg-6 order-1 order-lg-2 home-img">
-                  <img src={web} className="img-fluid animated" alt="" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Common
+        name="A much better digital experience with"
+        imgSrc={web}
+        visit="/service"
+        btName="Get Started"
+      />
     </>
   );
 };
