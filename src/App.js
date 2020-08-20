af22b0c1920e708121860c17c76c97de7219ba97
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-08-20 18:21:42 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+//bootstrap import
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+// component
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+import Service from './Components/Service';
+import Contact from './Components/Contact';
+import Navbar from './Components/Navbar/Navbar';
 
+// styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Animation</h1>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/service" component={Service} />
+        <Route exact path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
