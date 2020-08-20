22154e8615a0cfb8f2c2239c2490b2bd49747f2a
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-08-20 18:20:46 +0200

    Holds the props for card

---
+++ src/Components/CardDetails.jsx
@@ -0,0 +1,36 @@
+// images
+import digital from '../images/photo1.jpeg';
+import app from '../images/photo2.jpeg';
+import web from '../images/photo3.jpeg';
+import graphic from '../images/photo4.jpg';
+import software from '../images/photo5.jpeg';
+import android from '../images/photo6.jpeg';
+
+const CardDetails = [
+  {
+    imgSrc: digital,
+    title: 'Digital Marketing',
+  },
+  {
+    imgSrc: app,
+    title: 'App Development',
+  },
+  {
+    imgSrc: web,
+    title: 'Web Development',
+  },
+  {
+    imgSrc: graphic,
+    title: 'Graphic Design',
+  },
+  {
+    imgSrc: software,
+    title: 'Software Development',
+  },
+  {
+    imgSrc: android,
+    title: 'Android Development',
+  },
+];
+
+export default CardDetails;
